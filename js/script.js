de314530766c921ea2a8957906e5a2fbd4e41f16
Mikolaj Ozga <mikolaj.ozga03@gmail.com>
2023-01-10 15:45:57 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Cześć!");
+
+let button = document.querySelector(".js-section__button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".js-section__span");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("ciemny");
+
+    if (body.classList.contains("ciemny")) {
+        themeName.innerText = "biały";
+    } else {
+        themeName.innerText = "ciemny";
+    }
+})
