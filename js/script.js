ba885b7d6821fb56cf913694f6073db6df6a6609
Mikolaj Ozga <mikolaj.ozga03@gmail.com>
2023-05-01 18:25:16 +0200

    js refactoring

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-
-
-let button = document.querySelector(".js-section__button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-section__span");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("ciemny");
-
-    if (body.classList.contains("ciemny")) {
-        themeName.innerText = "biały";
-    } else {
-        themeName.innerText = "ciemny";
+{
+    const welcome = () => {
+        console.log("Witam wszystkich ciekawskich! :D");
     }
-})
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".js-section__span");
+
+        body.classList.toggle("ciemny");
+
+        if (body.classList.contains("ciemny")) {
+            themeName.innerText = "biały";
+        } else {
+            themeName.innerText = "ciemny";
+        }
+    }
+
+    const init = () => {
+        const button = document.querySelector(".js-section__button");
+
+        button.addEventListener("click", toggleBackground);
+
+        welcome();
+    };
+
+    init();
+}
